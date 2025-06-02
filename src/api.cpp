84339972edeaa29c891aa73b2ef87728e615de21
xsoder <tadihailukebe@gnail.com>
2025-06-02 22:33:39 +0000

    Updates for render branch

--- src/api.cpp
+++ src/api.cpp
@@ -2,20 +2,26 @@
 #include "util.hpp"
 #include <lauxlib.h>
 #include <lualib.h>
+#include <iostream>
 
 const char* filename = "(HOME)/.config/nwm/nwm.lua";
 
-void resize_window(struct application::app&){
-    todo();
+void resize_window(struct application::app& apps){
+    std::cout << "Fix this issue" << std::endl;
 }
 
-void load_config(struct application::app&){
-    todo();
+void load_config(struct application::app& apps){
+    std::cout << "Fix this issue" << std::endl;
+    //todo();
 }
 void load_lua(struct application::app& apps){
+    std::cout << "Fix this issue" << std::endl;
     apps.state = luaL_newstate();
     luaL_loadfilex(apps.state, filename, NULL);
 }
-void print(struct application::app&){
-    todo();
+void print(struct application::app& apps){
+    std::cout << "Fix this issue" << std::endl;
+    apps.state = luaL_newstate();
+    luaL_loadfilex(apps.state, filename, NULL);
+    //todo();
 }
