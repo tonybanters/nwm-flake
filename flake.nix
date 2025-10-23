31ae4ee333710099ebcc558c7f111b21b6eb1363
tonybtw <tonybtw@tonybtw.com>
2025-10-22 21:21:09 -0700

    updated flake.nix to proivde default value and be a windowmanager trait

--- flake.nix
+++ flake.nix
@@ -7,104 +7,93 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        
-        nwm = pkgs.stdenv.mkDerivation {
-          pname = "nwm";
-          version = "0.1.0";
-          src = ./.;
-          
-          buildInputs = with pkgs; [
-            xorg.libX11
-            xorg.libXft
-            xorg.libXrender
-            fontconfig
-            freetype
-          ];
-          
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-          ];
-          
-          makeFlags = [
-            "PREFIX=$(out)"
-            "CC=${pkgs.stdenv.cc.targetPrefix}cc"
-          ];
-          
-          installPhase = ''
-            mkdir -p $out/bin
-            install -Dm755 nwm $out/bin/nwm
-          '';
-          
-          meta = with pkgs.lib; {
-            description = "A scrollable tiling window manager for X11";
-            homepage = "https://github.com/xsoder/nwm";
-            license = licenses.mit;
-            platforms = platforms.linux;
-            maintainers = [ ];
+    let
+      nixosModulesOutput = {
+        nixosModules.default = { config, lib, pkgs, ... }:
+          with lib;
+          let
+            cfg = config.services.xserver.windowManager.nwm;
+          in {
+            options.services.xserver.windowManager.nwm = {
+              enable = mkEnableOption "nwm window manager";
+              package = mkOption {
+                type = types.package;
+                default = self.packages.${pkgs.system}.default;
+                description = "nwm package to use";
+              };
+            };
+
+            config = mkIf cfg.enable {
+              services.xserver.windowManager.session = [{
+                name = "nwm";
+                start = ''
+                  ${cfg.package}/bin/nwm &
+                  waitPID=$!
+                '';
+              }];
+              environment.systemPackages = [ cfg.package ];
+            };
           };
-        };
-        
-      in
-      {
-        packages = {
-          default = nwm;
-          nwm = nwm;
-        };
-        
-        devShells.default = pkgs.mkShell {
-          name = "nwm-dev";
-          
-          packages = with pkgs; [
-            # Build tools
-            pkg-config
-            gcc
-            gnumake
-            git
-            # X11 libraries
-            xorg.libX11
-            xorg.libXft
-            xorg.libXrender
-            # Font libraries
-            fontconfig
-            freetype
-            # Fonts
-            nerd-fonts.iosevka
-            # Debugging and development
-            gdb
-            valgrind
-            # Testing tools
-            xorg.xorgserver
-            xorg.xinit
-            xorg.xev
-            xorg.xdpyinfo
-            # Utilities
-            feh
-            st
-            dmenu
-          ];
-          
-          XINERAMA = "1";
-          FREETYPEINC = "${pkgs.freetype.dev}/include/freetype2";
-          X11INC = "${pkgs.xorg.libX11.dev}/include";
-          X11LIB = "${pkgs.xorg.libX11}/lib";
-        };
-        
-        # Minimal dev shell
-        devShells.minimal = pkgs.mkShell {
-          packages = with pkgs; [
-            pkg-config
-            xorg.libX11
-            xorg.libXft
-            xorg.libXrender
-            fontconfig
-            freetype
-            gcc
-            gnumake
-          ];
-        };
-      }
-    );
+      };
+
+      systemOutputs = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          nwm = pkgs.callPackage ./default.nix { };
+
+        in
+        {
+          packages = {
+            default = nwm;
+            nwm = nwm;
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "nwm-dev";
+
+            packages = with pkgs; [
+              pkg-config
+              gcc
+              gnumake
+              git
+              xorg.libX11
+              xorg.libXft
+              xorg.libXrender
+              fontconfig
+              freetype
+              nerd-fonts.iosevka
+              gdb
+              valgrind
+              xorg.xorgserver
+              xorg.xinit
+              xorg.xev
+              xorg.xdpyinfo
+              feh
+              st
+              dmenu
+            ];
+
+            XINERAMA = "1";
+            FREETYPEINC = "${pkgs.freetype.dev}/include/freetype2";
+            X11INC = "${pkgs.xorg.libX11.dev}/include";
+            X11LIB = "${pkgs.xorg.libX11}/lib";
+          };
+
+          devShells.minimal = pkgs.mkShell {
+            packages = with pkgs; [
+              pkg-config
+              xorg.libX11
+              xorg.libXft
+              xorg.libXrender
+              fontconfig
+              freetype
+              gcc
+              gnumake
+            ];
+          };
+        }
+      );
+    in
+      nixosModulesOutput // systemOutputs;
 }
